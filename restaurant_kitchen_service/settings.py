4b217a65af0cbb32358efd712727623450844dc4
Kitazuka <ilya.artnaev@gmail.com>
2022-11-11 01:26:59 +0200

    Add simple login and logout pages

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = "kitchen.Cook"
 
+LOGIN_REDIRECT_URL = "/"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
